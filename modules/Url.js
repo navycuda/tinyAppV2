2e3506fd6996662888931747d7cf359a1cbc72c9
NavyCuda <navysupra@gmail.com>
2022-08-17 01:36:29 -0700

    Improving Url class
    	Since I'm here and it's easy enough in this rewrite, adding
    	the stretch goal components in.

--- modules/Url.js
+++ modules/Url.js
@@ -10,7 +10,31 @@ class Url {
    */
   constructor(url, uid, database) {
     this.id = generateNewKey(6, database);
-    this.url = url;
+    this._url = url;
     this.owner = uid;
+    this.createdAt = Date.now();
+    this.visitors = {};
+    this.redirects = 0;
+  }
+  /**
+   * Tests to see if the user owns this url or not.
+   * @param {object} user The user object to check against ownership
+   * @returns bool
+   */
+  isOwnedBy(user) {
+    return this.owner === user.uid;
+  }
+  getUrlForRedirection(request) {
+    const ip = request.headers['x-forwarded-for'] || request.connection.remoteAddress;
+    if (!this.visitors[ip]) {
+      this.visitors[ip] = 1;
+    } else {
+      this.visitors[ip]++;
+    }
+    this.redirects++;
+    return this._url;
+  }
+  get urlForInspection() {
+    return this._url;
   }
 }
