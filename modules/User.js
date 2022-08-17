4d6746be738de183703fe92f28a3765f21730c2e
NavyCuda <navysupra@gmail.com>
2022-08-17 09:49:13 -0700

    Added method to check password

--- modules/User.js
+++ modules/User.js
@@ -1,10 +1,18 @@
 const { generateNewKey } = require("./helpers");
+const bcrypt = require('bcryptjs');
+
 
 class User {
   constructor(email, password, database) {
     this.uid = generateNewKey(6, database);
     this.email = email;
     this.password = password;
+  }
+  correctPassword(password) {
+    bcrypt.compare(password, this.password)
+      .then((result) => {
+        return result;
+      });
   }
 }
 
