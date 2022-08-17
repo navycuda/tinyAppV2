dba0cec6a3429146e45784187725ff81dda769bf
NavyCuda <navysupra@gmail.com>
2022-08-17 13:52:19 -0700

    Moved all password handling to User

--- modules/User.js
+++ modules/User.js
@@ -5,23 +5,26 @@ const bcrypt = require('bcryptjs');
 class User {
   constructor(email, database) {
     this.uid = generateNewKey(6, database);
+    this._dB = database;
     this.email = email;
   }
   correctPassword(password) {
-    bcrypt.compare(password, this.password)
+    bcrypt.compare(password, this._password)
       .then((result) => {
         return result;
       });
   }
   setPassword(password) {
-
-
-    return 'hashed password should be here';
-  }
-  isCorrect(password) {
-
-    
-    return false;
+    bcrypt.genSalt(10)
+      .then((salt) => {
+        return bcrypt.hash(password, salt);
+      })
+      .then((hash) => {
+        this._password = hash;
+        if (!this._dB[this.uid]) {
+          this._dB[this.uid] = this;
+        }
+      });
   }
 }
 
